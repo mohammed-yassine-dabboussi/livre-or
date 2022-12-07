271a713cdade7695de951c7e1ac2e4dea2f270d9
mohammed-yassine-dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-12-07 09:47:08 +0100

    structuration des pages html

---
+++ connect.php
@@ -0,0 +1,4 @@
+<?php 
+$mysqli = new mysqli('localhost', 'root', '', 'livreor');
+// $mysqli = new mysqli('localhost:3306', 'yassine', 'yassine123', 'mohammed-yassine-dabboussi_moduleconnexion');
+?>
